1f55a159cb7c5e5373415d7a9908ac3e229e4c09
Dionny <dfernand@ucab.edu.ve>
2024-02-22 14:18:16 -0400

    endpoint

--- init.go
+++ init.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "io"
+    "log"
+    "net/http"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+
+    http.HandleFunc("/", ExampleHandler)
+
+    log.Println("** Service Started on Port 8080 **")
+    err := http.ListenAndServe(":8080", nil);
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "application/json")
+    io.WriteString(w, `{"status":"ok"}`)
 }
